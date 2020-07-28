c4b31aa330f37f58bb30d4c7e2c2c60ad6c6df8f
Aarez <aarezahid@gmail.com>
2020-07-27 22:07:54 -0400

    Initialize chrome driver

---
+++ Bot.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import time , random
+
+shoe = input ("Enter link to Shoe")
+size = input ("Enter your size")
+
+browser = webdriver.Chrome()
+
+#TODO start with add to cart mens shoes from courtsidesneakers.com
+
+
+
+
+
+
+
+
+
+
+
+
+
+
